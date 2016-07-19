29cf5f9e0b8a5b0672e1a8e6a19734573c89994a
Molly <mollykmcglone@gmail.com>
2016-07-18 21:04:15 -0700

    pass all specs except Client.find_by_stylist_id and integration specs client path, update delete and assign

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -49,6 +49,30 @@ describe('#name') do
     end
   end
 
+  describe('.find_by_id') do
+    it 'locates clients with a given id' do
+      test_client = Client.new({:id => 3, :name => 'Patsy Cline',:phone => '503-250-2173', :email => 'patsycline@gmail.com', :stylist_id => nil})
+      test_client2 = Client.new({:id => 4, :name => 'Dolly Parton',:phone => '971-554-4455', :email => 'dollyparton@gmail.com', :stylist_id => nil})
+      test_client.save()
+      test_client2.save()
+      expect(Client.find_by_id(test_client.id)).to(eq(test_client))
+    end
+  end
+
+  describe('.find_by_stylist_id') do
+    it 'locates clients with a given stylist_id' do
+      test_stylist = Stylist.new({:id => nil, :name => 'Chris Damora',:station => '4'})
+      test_stylist.save()
+      test_stylist2 = Stylist.new({:id => nil, :name => 'Max Wells',:station => '3'})
+      test_stylist2.save()
+      test_client = Client.new({:id => nil, :name => 'Patsy Cline',:phone => '503-250-2173', :email => 'patsycline@gmail.com', :stylist_id => test_stylist.id()})
+      test_client.save()
+      test_client2 = Client.new({:id => nil, :name => 'Dolly Parton',:phone => '971-554-4455', :email => 'dollyparton@gmail.com', :stylist_id => test_stylist2.id()})
+      test_client2.save()
+      expect(Client.find_by_stylist_id(test_stylist.id())).to(eq([test_client]))
+    end
+  end
+
   describe('#update') do
     it 'lets the user update some of the attributes of the client' do
       test_client = Client.new({:id => nil, :name => 'Patsy Cline',:phone => '503-250-2173', :email => 'patsycline@gmail.com', :stylist_id => 3})
@@ -67,6 +91,15 @@ describe('#name') do
       test_client2.save()
       test_client.delete()
       expect(Client.all()).to(eq([test_client2]))
+    end
+  end
+
+  describe('#assign') do
+    it 'lets the user assign the client to a stylist' do
+      test_client = Client.new({:id => nil, :name => 'Patsy Cline',:phone => '503-250-2173', :email => 'patsycline@gmail.com', :stylist_id => nil})
+      test_client.save()
+      test_client.assign(4)
+      expect(test_client.stylist_id()).to(eq(4))
     end
   end
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -8,7 +8,7 @@ set(:show_exceptions, false)
 describe 'the stylist path', {:type => :feature} do
   it "gets to the Manage Stylists page" do
     visit '/'
-    click_link "Manage Stylists"
+    click_link "stylists"
     expect(page).to have_content("Manage Your Stylists")
   end
 
@@ -21,10 +21,21 @@ describe 'the stylist path', {:type => :feature} do
     expect(page).to have_content("Chris Zamora")
   end
 
+  it "allows the user to view info about a stylist" do
+    visit '/stylists'
+    click_link 'Add a New Stylist'
+    fill_in('name', :with => 'Chris Zamora')
+    fill_in('station', :with => '2')
+    click_button('Add Stylist')
+    click_link('View Details')
+    expect(page).to have_content("Stylist Information")
+  end
+
   it "allows the user to update a stylist" do
     stylist = Stylist.new({:id => nil, :name => 'Chris Damora',:station => '4'})
     stylist.save()
     visit '/stylists'
+    click_link('View Details')
     click_link('Edit or Delete')
     fill_in('Station', :with => '12')
     click_button('Update')
@@ -35,6 +46,7 @@ describe 'the stylist path', {:type => :feature} do
     stylist = Stylist.new({:id => nil, :name => 'Chris Damora',:station => '4'})
     stylist.save()
     visit '/stylists'
+    click_link('View Details')
     click_link('Edit or Delete')
     click_button('Delete this stylist')
     expect(page).to have_no_content('Chris Damora')
@@ -44,28 +56,34 @@ end
 describe 'the client path', {:type => :feature} do
   it "gets to the Manage Clients page" do
     visit '/'
-    click_link 'Manage Clients'
+    click_link 'clients'
     expect(page).to have_content('Manage Your Clients')
   end
 
   it "allows the user to add a client" do
+    stylist = Stylist.new({:id => nil, :name => 'Chris Damora',:station => '4'})
+    stylist.save()
     visit '/clients'
     click_link 'Add a New Client'
     fill_in('name', :with => 'Dolly Parton')
     fill_in('phone', :with => '503-250-2173')
     fill_in('email', :with => 'dollyparton@gmail.com')
+    select('Chris Damora', :from => 'stylist_id')
     click_button('Add Client')
     expect(page).to have_content('Dolly Parton')
   end
 
   it "allows the user to update a client's info" do
-    client = Client.new({:id => nil, :name => 'Dolly Parton',:phone => '503-250-2173', :email => 'dollyparton@gmail.com', :stylist_id => nil})
+    stylist = Stylist.new({:id => nil, :name => 'Chris Damora',:station => '4'})
+    stylist.save()
+    client = Client.new({:id => nil, :name => 'Dolly Parton',:phone => '503-250-2173', :email => 'dollyparton@gmail.com', :stylist_id => stylist.id()})
     client.save()
     visit '/clients'
-    click_link('Edit or Delete')
+    click_link('View Details')
+    click_link('Edit or Delete Client')
     fill_in("name", :with => 'Loretta Lynn')
     click_button('Update')
-    expect(page).to have_content('Manage Your Clients')
+    expect(page).to have_content('Loretta Lynn')
   end
 
   it "allows the user to assign a client to a stylist" do
@@ -74,12 +92,11 @@ describe 'the client path', {:type => :feature} do
     client.save()
     stylist.save()
     visit '/clients'
-    click_link('Edit or Delete')
+    click_link('Edit or Delete Client')
     select 'Chris Damora', :from => 'stylists'
-    click_button 'Assign Stylist'
+    click_button 'Update'
     expect(page).to have_content('Chris Damora')
   end
-
 
   it "allows the user to delete a client" do
     client = Client.new({:id => nil, :name => 'Dolly Parton',:phone => '503-250-2173', :email => 'dollyparton@gmail.com', :stylist_id => nil})
